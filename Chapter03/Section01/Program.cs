683370a9a54921a3f966bf668fb8f909c669ba23
HashimotoRiku <oibc35240@ojs.ac.jp>
2024-05-15 11:48:57 +0900

    ラムダ式

---
+++ Chapter03/Section01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 0, 10, 1, 5, };
+
+            double num = numbers.Where(n => n > 5).Average();
+            Console.WriteLine(num);
+
+        }
+    }
+}
