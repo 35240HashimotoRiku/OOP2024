1e4bb6faa4107d23866134b816ec92a6557c64a6
HashimotoRiku <oibc35240@ojs.ac.jp>
2024-05-15 15:08:40 +0900

    演習問題

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -8,11 +8,21 @@ using System.Threading.Tasks;
 namespace Section01 {
     internal class Program {
         static void Main(string[] args) {
-            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 0, 10, 1, 5, };
-
-            double num = numbers.Where(n => n > 5).Average();
-            Console.WriteLine(num);
-
+            var names = new List<string> {
+                "Tokyo",
+                "New Delhi",
+                "Bangkok",
+                "London",
+                "Paris",
+                "Berlin",
+                "Canberra",
+                "Hong Kong",
+};
+            IEnumerable<string> query = names.Where(s => s.Contains(""))
+                                                        .Select(s=>s.ToUpper());
+            foreach (var s in query) 
+                Console.WriteLine(s);
         }
+        
     }
 }
---
+++ Chapter03/演習問題/Exercize01/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercize01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var numbers = new List<int> { 12, 87, 94, 14, 53, 20, 40, 35, 76, 91, 31, 17, 48 };
+
+            // 3.1.1
+            Exercise1_1(numbers);
+            Console.WriteLine("-----");
+            // 3.1.2
+            Exercise1_2(numbers);
+            Console.WriteLine("-----");
+            // 3.1.3
+            Exercise1_3(numbers);
+            Console.WriteLine("-----");
+            // 3.1.4
+            Exercise1_4(numbers);
+        }
+
+        private static void Exercise1_1(List<int> numbers) {
+            var exists = numbers.Exists(s => s % 8 == 0|| s % 9 == 0);
+            Console.WriteLine(exists);
+        }
+
+        private static void Exercise1_2(List<int> numbers) {
+            var halfNum = numbers.ConvertAll(s => s / 2.0);
+            halfNum.ForEach(s => Console.WriteLine(s));
+        }
+
+        private static void Exercise1_3(List<int> numbers) {
+
+        }
+
+        private static void Exercise1_4(List<int> numbers) {
+
+        }
+    }
+}
