198cb9cc36f66071a7bde1aa2cf168f35e65e58e
HashimotoRiku <oibc35240@ojs.ac.jp>
2024-06-05 13:23:14 +0900

    第六章演習問題

---
+++ Chapter06/Section01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+
+           var pages = Books.GetBooks().OrderByDescending(x => x.Price).ToList();
+            foreach (var books in pages) {
+                Console.WriteLine(books.Price);
+            }
+        }
+    }
+}
