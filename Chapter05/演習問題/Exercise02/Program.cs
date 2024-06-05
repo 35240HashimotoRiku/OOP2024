4543856a4bc09564bc8272a602e070ad8acc4096
HashimotoRiku <oibc35240@ojs.ac.jp>
2024-06-05 10:23:15 +0900

    第五章演習問題完成

---
+++ Chapter05/演習問題/Exercise02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            var line = Console.ReadLine();
+            int num;
+            if (int.TryParse(line, out num)) {
+                Console.WriteLine($"{num:#,#}");
+                //Console.WriteLine("{0:#,#}", num);
+            } else {
+                Console.WriteLine("数字文字列でありません");
+            }
+        }
+    }
+}
