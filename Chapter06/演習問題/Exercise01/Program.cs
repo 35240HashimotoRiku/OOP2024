b6e05058e8529d4d6311712e5d8d91c8653c9b51
HashimotoRiku <oibc35240@ojs.ac.jp>
2024-06-05 13:41:45 +0900

    6-1-2完成

--- Chapter06/演習問題/Exercise01/Program.cs
+++ Chapter06/演習問題/Exercise01/Program.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace Exercise01 {
     internal class Program {
         static void Main(string[] args) {
             var numbers = new int[] { 5, 10, 17, 9, 3, 21, 10, 40, 21, 3, 35 };
-          
 
             Exercise1_1(numbers);
             Console.WriteLine("-----");
@@ -25,23 +25,27 @@ namespace Exercise01 {
             Exercise1_5(numbers);
         }
 
-        private static void Exercise1_4(int[] numbers) {
-            throw new NotImplementedException();
+        private static void Exercise1_1(int[] numbers) {
+            var maxNum = numbers.Max();
+            Console.WriteLine("最大値：" + maxNum);
+        }
+
+        private static void Exercise1_2(int[] numbers) {
+            var selected = numbers.Skip(numbers.Length - 2).ToArray();
+            foreach (var num in selected) {
+                Console.WriteLine(num);
+            }
         }
 
         private static void Exercise1_3(int[] numbers) {
             throw new NotImplementedException();
         }
 
+        private static void Exercise1_4(int[] numbers) {
+            throw new NotImplementedException();
+        }
+
         private static void Exercise1_5(int[] numbers) {
-            throw new NotImplementedException();
-        }
-
-        private static void Exercise1_2(int[] numbers) {
-            throw new NotImplementedException();
-        }
-
-        private static void Exercise1_1(int[] numbers) {
             throw new NotImplementedException();
         }
     }
