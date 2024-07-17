9535ff7222b246a8fbc3e17253a47613e57817f5
HashimotoRiku <oibc35240@ojs.ac.jp>
2024-07-17 14:57:04 +0900

    演習問題01

---
+++ Chapter11/Section01-1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var xdoc = XDocument.Load("novelists.xml");
+            var titles = xdoc.Root.Descendants("title");  
+
+            foreach ( var xnovelist in titles ) {
+                var xname = xnovelist.Element("name"); //要素の取得
+                var xworks = xnovelist.Element("masterpieces")
+                            .Elements("title")
+                            .Select(x => x.Value);
+
+                Console.WriteLine("{0} -{1}",xname.Value,string.Join(",",xworks));
+            }
+        }
+    }
+}
