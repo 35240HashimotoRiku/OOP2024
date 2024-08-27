c4f8e0f1b30faddfbff25ce511bfa0a811ac068f
HashimotoRiku <oibc35240@ojs.ac.jp>
2024-08-27 11:51:09 +0900

    演習問題1-2

---
+++ Chapter12/演習問題/Exercise01/Program.cs
@@ -0,0 +1,64 @@
+﻿using Section01;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            Exercise1_1("employee.xml");
+
+            // これは確認用
+            Console.WriteLine(File.ReadAllText("employee.xml"));
+            Console.WriteLine();
+
+            Exercise1_2("employees.xml");
+            Exercise1_3("employees.xml");
+            Console.WriteLine();
+
+            Exercise1_4("employees.json");
+
+            // これは確認用
+            Console.WriteLine(File.ReadAllText("employees.json"));
+        }
+
+        private static void Exercise1_1(string outfile) {
+            var emp = new Employee {
+                Id = 456,
+                Name = "次郎",
+                HireDate = new DateTime(2004, 12, 1)
+            };
+            using(var writer = XmlWriter.Create(outfile)) {
+                var serializer = new XmlSerializer(emp.GetType());
+                serializer.Serialize(writer, emp);
+            }
+        }
+
+        private static void Exercise1_2(string outfile) {
+            var emps = new Employee[] {
+                new Employee {
+                    Id = 123,
+                    Name = "太郎",
+                    HireDate = DateTime.Now
+                },
+                new Employee {
+                Id = 456,
+                Name = "次郎",
+                HireDate = new DateTime(2004, 12, 1)
+                },
+            };
+        }
+            
+
+        private static void Exercise1_3(string v) {
+        }
+
+        private static void Exercise1_4(string v) {
+        }
+    }
+}
