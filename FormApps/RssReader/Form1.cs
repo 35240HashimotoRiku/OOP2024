d36c67f2001d4e87e851ea0727b75b99c772f433
HashimotoRiku <oibc35240@ojs.ac.jp>
2024-09-03 11:24:30 +0900

    Rss表示

---
+++ FormApps/RssReader/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace RssReader {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btGet_Click(object sender, EventArgs e) {
+            using (var wc = new WebClient()) {
+                var url = wc.OpenRead(tbRssUrl.Text);
+                var xdoc = XDocument.Load(url);
+
+                var xelements = xdoc.Root.Descendants("item");
+                foreach (var xelement in xelements) {
+                    var novelists = xelement.Element("title");
+                        lbRssTitle.Items.Add(novelists);
+                }
+            }
+        }
+    }
+}
