5cc7b11d230d957392b7f9a259ec5df4e6c6afb9
HashimotoRiku <oibc35240@ojs.ac.jp>
2024-10-16 13:46:18 +0900

    17-1完成

---
+++ Chapter17/TemplateMethod/DistanceConverter/ConverterFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceConverter {
+    static class ConverterFactory {
+
+        private static ConverterBase[] _converters = new ConverterBase[] {
+            new MeterConverter(),
+            new FeetConverter(),
+            new YardConverter(),
+            new InchConverter(),
+            new MileConverter(),
+            new KilometerConverter(),
+        };
+
+        public static ConverterBase getInstance(string name) {
+            return _converters.FirstOrDefault(x => x.IsMyUnit(name));
+        }
+    }
+}
---
+++ Chapter17/TemplateMethod/TextNumberSizeChange/ToHankakuProcessor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextFileProcessor;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace TextNumberSizeChange {
+    class ToHankakuProcessor : TextProcessor{
+        // 変換テーブルをつくる
+        Dictionary<char,char> conv = new Dictionary<char,char>() {
+    {'１','1'},{'２','2'},{'３','3'},{'４','4'},{'５','5'},
+    {'６','6'},{'７','7'},{'８','8'},{'９','9'},{'０','0'},
+            };
+        
+        protected override void Execute(string line) {
+            string s = new string(line.Select(n => (conv.ContainsKey(n) ? conv[n] : n)).ToArray());
+
+            Console.WriteLine(s); // 出力結果を表示
+
+        }
+
+    }
+}
