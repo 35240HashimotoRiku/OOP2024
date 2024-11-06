89f7fe2b6f11a8567670db612ab9089a87ec9dd8
HashimotoRiku <oibc35240@ojs.ac.jp>
2024-11-06 14:22:05 +0900

    SampleConverter

--- WPF/CollorChecker/MainWindow.xaml.cs
+++ WPF/CollorChecker/MainWindow.xaml.cs
@@ -21,12 +21,13 @@ namespace CollorChecker {
     /// </summary>
     public partial class MainWindow : Window {
         MyColor currentColor = new MyColor(); //現在設定している色情報
+        MyColor[] colorsTable; //色のデータ
 
         public MainWindow() {
             InitializeComponent();
             //αチャンネルの初期値を設定（起動時すぐにストックボタンが押された場合の対応）
             // currentColor.Color = Color.FromArgb(255,0,0,0);
-            DataContext = GetColorList();
+            DataContext = colorsTable = GetColorList();
         }
         //MyColor currentColor = new MyColor();
 
@@ -49,8 +50,23 @@ namespace CollorChecker {
             // int bvalue = (int) bSlider.Value;
             // colorArea.Background = new SolidColorBrush(Color.FromRgb((byte)rvalue, (byte)gvalue, (byte)bvalue));
             currentColor.Color = Color.FromRgb((byte)rSlider.Value, (byte)gSlider.Value, (byte)bSlider.Value);
-            //currentColor.Name = GetColorList().Where(c => c.Equals(currentColor.Color)).;
+            //currentColor.Name = colorsTable.Where(c => c.Equals(currentColor.Color)).Select(x=>x.Name).FirstOrDefault();
+
+            int i;
+            for (i = 0; i< colorsTable.Length; i++) {
+                if (colorsTable[i].Color.Equals(currentColor.Color)) {
+                    currentColor.Name = colorsTable[i].Name;
+                    break;
+                }
+            }
+            if (i == colorsTable.Length) {
+                ColorSelctComboBox.SelectedIndex = i;
+            } else {
+                ColorSelctComboBox.SelectedIndex = 0;
+            }
+            ColorSelctComboBox.SelectedIndex = i;
             colorArea.Background = new SolidColorBrush(currentColor.Color);
+
         }
 
         private void STOCK_Click(object sender, RoutedEventArgs e) {
